e68f3c61ef8a00fd8adb7a1fdd49af7cd296cb71
Nidhi Chaudhary <mynetworknidhi02@gmail.com>
2025-05-05 13:38:56 +0530

    adding workflow for refreshing cloudflare service token

--- scripts/list-service-token.py
+++ scripts/list-service-token.py
@@ -13,7 +13,6 @@ HEADERS = {
 def get_tokens():
     response = requests.get(f"{API_BASE}", headers=HEADERS)
     response.raise_for_status()
-    print(response.json())
     return response.json()
 
 def is_expiring_soon(expiry_date_str):
